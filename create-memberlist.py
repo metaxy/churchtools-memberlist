a1beb983e64d4b5a11d0dee8793ca670d6efc2c8
Paul Walger <metaxy@walger.name>
2024-02-07 11:20:52 +0100

    filter by status

--- create-memberlist.py
+++ create-memberlist.py
@@ -16,6 +16,8 @@ from py3o.template import Template
 parser = argparse.ArgumentParser()
 parser.add_argument("--filter-group", help="Filter for group ID")
 parser.add_argument("--filter-role", help="Filter for role ID")
+parser.add_argument("--filter-status", help="Filter by status")
+
 parser.add_argument("--template", default="template_memberlist.odt", help="custom template file (odt)")
 parser.add_argument("--output", default="memberlist.odt", help="output file (odt)")
 args = parser.parse_args()
@@ -24,7 +26,7 @@ args = parser.parse_args()
 t = Template(args.template, args.output)
 
 # Sort persons by their family
-persons_sorted = churchtoolsapi.get_persons(args.filter_group, args.filter_role, include_images=True)
+persons_sorted = churchtoolsapi.get_persons(args.filter_group, args.filter_role, args.filter_status.split(","), include_images=True)
 
 data = dict(persons=persons_sorted)
 t.render(data)
